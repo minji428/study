357f3a16f65e144c464e41cb723a084077e49c9b
minji428 <83818241+minji428@users.noreply.github.com>
2021-07-27 23:14:15 +0900

    Add files via upload

--- MHP/workspace/MyHealthPartner/src/java/mhp/memberDAO.java
+++ MHP/workspace/MyHealthPartner/src/java/mhp/memberDAO.java
@@ -1,5 +1,30 @@
 package mhp;
 
-public class memberDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+public class memberDAO {
+	private memberDAO() {}
+	public static memberDAO instance = new memberDAO();
+	public static memberDAO getInstance() {return instance;}
+	
+	Connection conn=null;
+	PreparedStatement pstmt = null;
+	ResultSet rs = null;
+	
+	public Connection getConnection() {
+		String dbURL = "jdbc:mysql://localhost:3306/ticket?serverTimezone=UTC";
+		String dbID = "root";
+		String dbPassword = "";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
 }
