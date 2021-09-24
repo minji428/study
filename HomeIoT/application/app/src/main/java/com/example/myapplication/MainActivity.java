4dba042be26eeced32efd2b0656af92941c6069c
minji428 <83818241+minji428@users.noreply.github.com>
2021-09-24 16:52:05 +0900

    Add files via upload

---
+++ HomeIoT/application/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClicked(View v){
+        setContentView(R.layout.button);
+        Toast.makeText(this,"버튼 눌림",Toast.LENGTH_LONG).show();
+    }
+}
