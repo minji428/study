ccf8f7e9606e363dacc6b64c50f64d994f1c38dc
jjjes758 <jjjes758@skuniv.ac.kr>
2021-10-03 13:06:54 +0900

    1003

--- JeonIoT/arduino/app/src/main/java/kr/ac/skuniv/arduino/MainActivity.java
+++ JeonIoT/arduino/app/src/main/java/kr/ac/skuniv/arduino/MainActivity.java
@@ -1,23 +1,29 @@
 package kr.ac.skuniv.arduino;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 
     public void onButtonClicked(View v){
         setContentView(R.layout.button);
-        Toast.makeText(this,"버튼 눌림",Toast.LENGTH_LONG).show();
     }
 
     public boolean onKeyDown(int keyCode, KeyEvent evnet){
