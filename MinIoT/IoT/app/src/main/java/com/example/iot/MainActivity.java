d51e32e47dda55fd89a7d1689ca3387dc193bdb9
minji428 <alswl980428@gmail.com>
2021-10-18 03:25:57 +0900

    20211015

---
+++ MinIoT/IoT/app/src/main/java/com/example/iot/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.iot;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    Button btn1,btn2,btn3,btn4,btn5,btn6;
+    static final int signal1=1;
+    static final int signal2=2;
+    static final int signal3=3;
+    static final int signal4=4;
+    static final int signal5=5;
+    static final int signal6=6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = (Button)findViewById(R.id.button);
+        btn2 = (Button)findViewById(R.id.button2);
+        btn3 = (Button)findViewById(R.id.button3);
+        btn4 = (Button)findViewById(R.id.button4);
+        btn5 = (Button)findViewById(R.id.button5);
+        btn6 = (Button)findViewById(R.id.button6);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,activity1.class);
+                startActivityForResult(intent,signal1);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(MainActivity.this,activity2.class);
+                startActivityForResult(intent2,signal2);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,activity3.class);
+                startActivityForResult(intent,signal3);
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,activity4.class);
+                startActivityForResult(intent,signal4);
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,activity5.class);
+                startActivityForResult(intent,signal5);
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,activity6.class);
+                startActivityForResult(intent,signal6);
+            }
+        });
+
+
+    }
+
+
+
+}
